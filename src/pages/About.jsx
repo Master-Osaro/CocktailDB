3a084a1c847a731b4482fcc7684947af905abdf4
Osaro Iyoha <saro.iyoha@gmail.com>
2022-10-03 04:31:17 +0100

    Add files via upload

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum veniam quas veritatis? Voluptate facilis repellat eos, maiores eum in esse, voluptas eveniet ratione fuga ut aspernatur, assumenda magnam deleniti iure!
+        Deserunt, unde! Ab sunt officiis quam hic aliquam adipisci maxime at. Qui neque pariatur, ab quaerat commodi molestiae cupiditate at quas optio nesciunt! Aspernatur explicabo error dolores natus, eos suscipit!
+      </p>
+    </section >
+  )
+}
+
+export default About
